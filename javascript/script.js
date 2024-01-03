2a03c333ae6dd895c7061280c82c00479c1c87c7
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-01-03 11:28:41 +0100

    Implement game logic and ui elements

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,22 @@
-let playerWins = 0;
-let computerWins = 0;
+let playerScore = 0;
+let computerScore = 0;
+const buttons = document.getElementsByTagName('button');
+const result = document.querySelector('.result');
 
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * options.length)];
 }
 
+function updateScore() {
+    document.querySelector('.player-score').innerText = playerScore + " - ";
+    document.querySelector('.computer-score').innerText = computerScore;
+
+}
+
 function playRound(playerSelection, computerSelection) {
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
-    console.log(player);
-    console.log(computer);
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
     if (player === computer) {
         return ("Draw! Pick again");
     } else {
@@ -27,11 +33,37 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const buttons = document.getElementsByTagName('button');
 
-Array.from(buttons).forEach(function (button) {
-    button.addEventListener('click', function () {
-        const result = playRound(this.textContent, getComputerChoice());
-        console.log(result);
+function roundWin(round) {
+    if (round.includes("win")) {
+        playerScore++;
+    } else if (round.includes("lose")) {
+        computerScore++;
+    }
+    updateScore();
+    if (playerScore === 5) {
+        setTimeout(() => {
+            alert("You win! Congratulations!");
+            playerScore = 0;
+            computerScore = 0;
+            updateScore();
+        }, 100);
+    } else if (computerScore === 5) {
+        setTimeout(() => {
+            alert("You lose! Try again!");
+            playerScore = 0;
+            computerScore = 0;
+            updateScore();
+        }, 100);
+    }
+}
+
+
+
+Array.from(buttons).forEach((button) =>{
+    button.addEventListener('click', () => {
+        const round = playRound(button.textContent, getComputerChoice());
+        result.innerText = round;
+        roundWin(round);
     });
 });
